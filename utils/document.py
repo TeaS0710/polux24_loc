d9427201c9b9bb0377b19c9de255f15c55aca201
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-24 21:34:50 +0100

    Update document.py

--- utils/document.py
+++ utils/document.py
@@ -1,1 +1,18 @@
+from dataclasses import dataclass
+from base import DataWrapper
 
+@dataclass(frozen=True, slots=True)
+class Document(DataWrapper):
+  author: str
+  group: str
+  text: str
+  url: str
+
+  @classmethod
+  def from_file(cls, author, group, path, url):
+    return cls(
+      author=author,
+      group=group,
+      text=get_text(path),
+      url=url
+    )
