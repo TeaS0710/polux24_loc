5924a593fac1c18bb180adec89e87907ba8a8148
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-24 21:45:18 +0100

    Update data_wrapper.py

--- utils/data_wrapper.py
+++ utils/data_wrapper.py
@@ -1,1 +1,42 @@
+from dataclasses import dataclass
+import msgpack
 
+from .text_extractor import get_text
+
+
+class DataWrapper:
+  def __init__(self):
+    pass
+  
+  def encode(self):
+    msgpack.packb(vars(self), use_bin_type=True)
+
+  @classmethod
+  def decode(cls, data):
+    return cls(**msgpack.unpackb(data, raw=False))
+    
+
+@dataclass(frozen=True, slots=True)
+class Document(DataWrapper):
+  author: str
+  group: str
+  text: str
+  url: str
+
+  @classmethod
+  def from_file(cls, author, group, path, url):
+    return cls(
+      author=author,
+      group=group,
+      text=get_text(path),
+      url=url
+    )
+
+
+@dataclass
+class LocEntity(DataWrapper):
+  document: str
+  end: int
+  motor: str
+  start: int
+  text: str
