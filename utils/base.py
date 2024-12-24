71dd266a9bc4a3b718b4880cdb0a024e78c51402
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-24 21:24:08 +0100

    Update base.py

--- utils/base.py
+++ utils/base.py
@@ -1,1 +1,12 @@
-class
+import msgpack
+
+class DataWrapper:
+  def __init__(self):
+    pass
+  
+  def encode(self):
+    msgpack.packb(vars(self), use_bin_type=True)
+
+  @classmethod
+  def decode(cls, data):
+    return cls(**msgpack.unpackb(data, raw=False))
