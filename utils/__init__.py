d5f88b7c295d73edfb5076fc5291967caead52db
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-25 04:07:46 +0100

    Update __init__.py
    update pour ajouter le logger

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from .document import Document
-from.loc_entity import LocEntity
+from .loc_entity import LocEntity
+from .new_logger import new_logger
 
-__all__ = ["Document", "LocEntity"]
+__all__ = ["Document", "LocEntity", "new_logger"]
