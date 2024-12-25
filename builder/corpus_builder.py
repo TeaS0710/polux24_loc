04e36544afbd086cad14335aaae8df4de4df4c57
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-25 03:14:22 +0100

    Update corpus_builder.py
    Ajout de clean_table

--- builder/corpus_builder.py
+++ builder/corpus_builder.py
@@ -1,7 +1,51 @@
 import lmdb
-from os.path import basename, splitext, exists
+from hashlib import md5
+from os.path import splitext, exists
 
 from utils import Document
+
+
+def compute_md5(utf8_str):
+    return md5(utf8_str.encode("utf-8")).hexdigest()
+
+
+def clean_table(hash_table, logger):
+    logger.info("Function 'clean_table' called.")
+    
+    cleaned_table = {}
+    initial_size = len(hash_table)
+    
+    for key, data in hash_table.items():        
+        match data:
+            case {"author": str(author), "group": str(group), "path": str(path), "url": str(url)}:
+                _, ext = splitext(path)
+                
+                if ext not in Document.supported_exts:
+                    logger.debug(f"Unsupported file extension: '{key}' skipped.")
+                    continue
+                
+                if not exists(path):
+                    logger.debug(f"File path does not exist: '{key}' skipped.")
+                    continue
+            
+                if key != compute_md5(url):
+                    logger.debug(f"Mismatch between 'hash' and 'url': '{key}' skipped.")
+                    continue
+                
+            case _:
+                logger.debug(f"Invalid data structure: '{keys}' skipped.")
+                continue
+        
+        cleaned_table[key] = data
+        logger.debug(f"Verified key added: '{key}' matches all the requirements.")
+    
+    cleaned_size = len(cleaned_table)
+    
+    if cleaned_size < initial_size:
+        logger.warning(f"{initial_size - cleaned_size} document(s) removed from the original hash table.")
+    
+    logger.info(f"Function 'clean_table' completed successfully.")
+    return cleaned_table
 
 
 def add_documents(txn, hash_table, logger):
