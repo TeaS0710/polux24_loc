779eb6a26331847ee802301e1cdcaf7f11efc4cf
TeaS0710 <161449251+TeaS0710@users.noreply.github.com>
2024-12-25 01:25:14 +0100

    Update tampon_STANZA.py

--- .trash/tampon_STANZA.py
+++ .trash/tampon_STANZA.py
@@ -1,34 +1,26 @@
 import stanza
-import json
 
 stanza.download('fr')
 nlp = stanza.Pipeline('fr', processors='tokenize,ner')
 
-def read_text(path):
-    with open(path_txt, mode='r', encoding='utf-8') as f:
-        return f.read()
+def extract_locations(key, txt):
 
-def extract_locations_to_json(text, author):
-    doc = nlp(text)
-    locations = {
-        f"localisation {i}": {
-            "Entité": ent.text,
-            "Label": ent.type
-        }
-        for i, ent in enumerate(doc.ents) if ent.type == "LOC"
-    }
+    doc = nlp(txt)
+    locentities = []
 
-    output_file = f"{author}_stanza_locations.json"
-    with open(output_file, mode='w', encoding='utf-8') as f:
-        json.dump(locations, f, indent=4)
-    print(f"Fichier JSON généré : {output_file}")
-    return output_file, author, location
+    for ent in doc.ents:
+        if ent.type == "LOC":
+            yield LocEntity(
+                document= key,
+                start= ent.start_char,
+                end= ent.end_char,
+                motor="stanza",
+                text= ent.text
+            )
+    return locentities
 
 def main():
-    for path in paths:
-        author = path.split("\\")[-3]
-        text = read_text(path)
-        print(f"Traitement : {path}")
-        output_file = extract_locations_to_json(text, author)
-        print(f"Nombre de localisations extraites dans {output_file}: {location_count}")
-main()
+    locations = extract_locations(key, txt)
+    print(locations)
+    
+    main()
