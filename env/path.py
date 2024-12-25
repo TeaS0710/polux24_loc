5c7e7333f6a9dd830b789117955834f7c631b7b0
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-25 03:49:21 +0100

    Update path.py
    ajout de .cache

--- env/path.py
+++ env/path.py
@@ -1,5 +1,6 @@
 from os.path import abspath
 
 class PATH:
-  CORPUS_PATH = abspath("../data/corpus")
-  HASH_TABLE_PATH = abspath("../data/source/metadata.json")
+  CORPUS = abspath("../data/corpus")
+  HASH_TABLE = abspath("../data/source/metadata.json")
+  CACHE = abspath("../.cache")
