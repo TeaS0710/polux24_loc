7ae8e32efd52fcd9543fa2072c7f773434a115bf
TeaS0710 <161449251+TeaS0710@users.noreply.github.com>
2024-12-25 01:37:21 +0100

    Update tampon_Flair.py

--- .trash/tampon_Flair.py
+++ .trash/tampon_Flair.py
@@ -1,38 +1,29 @@
 import flair
 from flair.data import Sentence
 from flair.models import SequenceTagger
-import json
 
 tagger = SequenceTagger.load('ner')
 
-def read_text(path_txt):
-    with open(path_txt, mode='r', encoding='utf-8') as f:
-        return f.read()
-
-def extract_locations_to_json(text, author):
+def extract_locations_to_json(key, txt):
     sentence = Sentence(text)
-    tagger.predict(sentence)
-    
-    locations = {
-        f"localisation {i}": {
-            "Entité": entity.text,
-            "Label": entity.get_label("ner").value
-        }
-        for i, entity in enumerate(sentence.get_spans('ner')) if entity.get_label("ner").value == "LOC"
-    }
+    nlp = tagger.predict(sentence)
+    doc = nlp(txt)
 
-    output_file = f"{author}_flair_locations.json"
-    with open(output_file, mode='w', encoding='utf-8') as f:
-        json.dump(locations, f, indent=4)
-    print(f"Fichier JSON généré : {output_file}")
-    return output_file, author, location
+    locentities = []
+
+    for ent in doc.ents:
+        if ent.type == "LOC":
+            yield LocEntity(
+                document= key,
+                start= ent.start_char,
+                end= ent.end_char,
+                motor=f"{model_lang}",
+                txt= ent.text
+            )
+    return locentities
 
 def main():
-    for path in paths:
-        author = path.split("\\")[-3]
-        text = read_text(path)
-        print(f"Traitement : {path}")
-        output_file = extract_locations_to_json(text, author)
-        print(f"Nombre de localisations extraites dans {output_file}: {location_count}")
-
+    locations = extract_locations(key, txt)
+    print(locations)
+    
 main()
