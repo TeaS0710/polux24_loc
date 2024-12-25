e649d103b3a933306e03663b41fe60efe767b514
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-25 02:42:47 +0100

    Update corpus_builder.py
    Simplification en factorisant un peu

--- builder/corpus_builder.py
+++ builder/corpus_builder.py
@@ -1,63 +1,43 @@
-import glob
 import lmdb
-
-from os import basename, splitext
+from os.path import basename, splitext, exists
 
 from utils import Document
 
 
-def built(db_path, hash_table, src_files_dir, logger):
-    logger.info("Function 'built' called.")
+def add_documents(txn, hash_table, logger):
+    logger.warning("Function 'built' called.")
     
-    added_docs, total_files, total_hashes = 0, 0, len(hash_table)
-
-    logger.debug("Opening LMDB environment.")
+    added_docs = 0
+    total = 0
     
-    env = lmdb.open(db_path, map_size=10485760)
-    polux24 = env.open_db(b"polux24")
+    txn.drop(delete=False)
+    logger.debug("Database cleared for rewriting.")
     
-    logger.debug(f"Database 'polux24' successfully opened in LMDB environment at '{db_path}'.")
-    
-    with env.begin(write=True) as txn:
-        logger.debug("Transaction opened in write mode.")
-    
-        txn.drop(polux24, delete=False)
-        logger.debug("Database 'polux24' cleared for rewriting.")
-
-        for path in glob.iglob(f"{src_files_dir}/**"):
-            logger.info(f"Processing file: {path}")
-            key, ext = splitext(basename(path))
-            total_files += 1
-
-            if key not in hash_table:
-                logger.error(f"Skipping document '{key}'. No entry found in hash table.")
-                continue
-
-            if ext not in Document.supported_exts:
-                logger.error(f"Skipping document '{key}'. Unsupported extension: '{ext}'.")
-                continue
-
-            try:
-                doc = Document.from_file(**hash_table[key], path=path)
-                logger.debug(f"Document '{key}' initialized successfully.")
-                
-                txn.put(key.encode(), doc.encode(), db=polux24)
-                logger.debug(f"Document '{key}' added to 'polux24' database.")
-                
-                logger.info(f"Document '{key}' processed successfully.")
-                added_docs += 1
-
-            except Exception as exception:
-                logger.error(f"Skipping document '{key}'. An error occurred: '{exception}'.")
-
-        logger.warning(f"Processing complete: {added_docs} document(s) successfully added to the database.")
+    for key, data in hash_table.items():
+        logger.info(f"Processing document with key: {key}")
         
-        if total_files != added_docs:
-            logger.warning(f"Mismatch between added document(s) and file(s): {added_docs} added document(s) vs {total_files} file(s).")
+        try:
+            doc = Document.from_file(**data)
+            logger.debug(f"Document '{key}' initialized successfully.")
         
-        if total_hashes != total_files:
-            logger.warning(f"Mismatch between hash(es) and file(s): {total_hashes} hash(es) vs {total_files} file(s).")
+            txn.put(key.encode(), doc.encode())
+            logger.debug(f"Document '{key}' added to database.")
+            
+            logger.info(f"Document '{key}' processed successfully.")
+            added_docs += 1
 
-    env.close()
-    logger.debug("Closing LMDB environment.")
-    logger.info("Function 'built' completed successfully.")
+        except Exception as exception:
+            logger.error(f"Skipping document '{key}'. An error occurred: '{exception}'.")
+
+        finally:
+            total += 1
+
+    logger.warning(f"Function 'built' completed: {added_docs}/{total} document(s) successfully added to the database.")
+
+
+def built(db_path, hash_table_path, logger):
+    pass
+    # Implémenter:
+    # Ouvre hash_table_path
+    # Ouvre l'env, polux24 et initie la transaction
+    # appelle add_documents
