92d2dc66cda760aea727a372eae103d4f08385ab
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-28 17:03:26 +0100

    Update locs_extractor.py

--- utils/locs_extractor.py
+++ utils/locs_extractor.py
@@ -1,78 +1,76 @@
 import spacy
 import stanza
-
 from flair.data import Sentence
 from flair.models import SequenceTagger
 
 
-
-SPACY_MODEL = spacy.load("trf_Precision")
-STANZA_MODEL = stanza.Pipeline("fr", processors="tokenize,ner")
-FLAIR_TAGGER = SequenceTagger.load("ner")
-
-def flair(text):
-    sentance = Sentence(text)
-    FLAIR_TAGGER.predict(sentance)
-    return sentance.get_spans("ner")
-
-def colander_flair(ent):
-    return entity.get_label("ner").value == "LOC"
-
-def __ner_spacy(text):
-    for ent in SPACY_MODEL(text).ents:
-        if ent.label_ == "LOC":
-            yield {
-                "location": ent.text,
-                "start": ent.start_char,
-                "end": ent.end_char
-            }
-
-
-def __ner_stanza(text):
-    for ent in __ner_stanza.model(text).ents:
-        if ent.type == "LOC":
-            yield {
-                "location": ent.text,
-                "start": ent.start_char,
-                "end": ent.end_char
-            }
+class NerModeWrapper:
+    model = None
     
+    def ents(self, text):
+        raise NotImplementedError
+
+    def filter(self, ent):
+        raise NotImplementedError
+
+    def ents_to_dict(self, ent):
+        raise NotImplementedError
+
+    def ner(self, text):
+        return [self.ents_to_dict(entity) for entity in self.ents(text) if self.filter(entity)]
 
 
-def __ner_flair(text):
-    tagger = SequenceTagger.load('ner')
-    sentence = Sentence(txt)
-    tagger.predict(sentence)
-    for entity in sentence.get_spans('ner'):
-        if entity.get_label("ner").value == "LOC":
-            yield LocEntity(
-                document=key,
-                start=entity.start_position,
-                end=entity.end_position,
-                motor="flair",
-                text=entity.text
-            )
+class SpacyAndStanzaBase(NerModeWrapper):
+    def ents(self, text):
+        # Utilisation de self.model au lieu de 'model'
+        return self.model(text).ents
+
+    def ents_to_dict(self, ent):
+        return {
+            "text": ent.text,
+            "start": ent.start_char,
+            "end": ent.end_char
+        }
+
+
+class SpacyNerModel(SpacyAndStanzaBase):
+    model = spacy.load("trf_Precision")
+
+    def filter(self, ent):
+        return ent.label_ == "LOC"
+
+
+class StanzaNerModel(SpacyAndStanzaBase):
+    model = stanza.Pipeline("fr", processors="tokenize,ner")
+
+    def filter(self, ent):
+        return ent.type == "LOC"
+
+
+class FlairNerModel(NerModeWrapper):
+    model = SequenceTagger.load("ner")
+    
+    def ents(self, text):
+        sentence = Sentence(text)
+        self.model.predict(sentence)
+        return sentence.get_spans("ner")
+
+    def filter(self, entity):
+        return entity.get_label("ner").value == "LOC"
+
+    def ents_to_dict(self, ent):
+        return {
+            "start": ent.start_position,
+            "end": ent.end_position,
+            "location": ent.text
+        }
 
 
 def get_locs(text):
-    return {motor: extractor(text) for motor, extractor in get_locs.extractors.items()}
+    return {motor_name: motor.ner(text) for motor_name, motor in get_locs.extractors.items()}
     
 get_locs.extractors = {
-    "flair": __ner_flair,
-    "spacy": __ner_spacy,
-    "stanza": __ner_stanza
-}
-
-
-"""
-
-"""
-
-def get_locs(text):
-    return {motor: extractor(text) for motor, extractor in get_locs.extractors.items()}
-    
-get_locs.extractors = {
-    "flair": __ner_flair,
-    "spacy": __ner_spacy,
-    "stanza": __ner_stanza
+    "flair": FlairNerModel(),
+    "spacy": SpacyNerModel(),
+    "stanza": StanzaNerModel()
 }
