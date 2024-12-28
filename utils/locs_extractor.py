3ac6a303274988e3ffc97a211c397485def9a7c2
TeaS0710 <161449251+TeaS0710@users.noreply.github.com>
2024-12-28 17:10:08 +0100

    Update locs_extractor.py

--- utils/locs_extractor.py
+++ utils/locs_extractor.py
@@ -3,6 +3,97 @@ import stanza
 from flair.data import Sentence
 from flair.models import SequenceTagger
 
+import stanza
+import flair
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+tagger = SequenceTagger.load('ner')
+
+def extract_locations_to_json(key, txt):
+    sentence = Sentence(text)
+    nlp = tagger.predict(sentence)
+    doc = nlp(txt)
+
+    locentities = []
+
+    for ent in doc.ents:
+        if ent.type == "LOC":
+            yield LocEntity(
+                document= key,
+                start= ent.start_char,
+                end= ent.end_char,
+                motor=f"{model_lang}",
+                text= ent.text
+            )
+    return locentities
+
+def main():
+    locations = extract_locations(key, txt)
+    print(locations)
+    
+main()
+
+def extract_locations(key, txt):
+
+    stanza.download('fr')
+    nlp = stanza.Pipeline('fr', processors='tokenize,ner')
+
+    doc = nlp(txt)
+    locentities = []
+
+    for ent in doc.ents:
+        if ent.type == "LOC":
+            yield LocEntity(
+                document= key,
+                start= ent.start_char,
+                end= ent.end_char,
+                motor="stanza",
+                text= ent.text
+            )
+    return locentities
+
+def main():
+    locations = extract_locations(key, txt)
+    print(locations)
+    
+    main()
+
+import spacy
+from spacy import displacy
+import json
+
+#python -m spacy download fr_dep_news_trf
+#python -m spacy download fr_core_news_lg
+
+Modele_version = "trf"
+if Modele_version == "lg_Efficiency":
+    Modele = "fr_core_news_lg"
+elif Modele_version == "trf_Precision":
+    Modele = "fr_dep_news_trf"
+
+def extract_locations(txt, model_lang, key):
+    nlp = spacy.load(model_lang)
+    doc = nlp(txt)
+
+    locentities = []
+
+    for ent in doc.ents:
+        if ent.type == "LOC":
+            yield LocEntity(
+                document= key,
+                start= ent.start_char,
+                end= ent.end_char,
+                motor=f"{model_lang}",
+                text= ent.text
+            )
+    return locentities
+
+def main():
+    locations = extract_locations(key, txt)
+    print(locations)
+    
+    main()
 
 class NerModeWrapper:
     model = None
