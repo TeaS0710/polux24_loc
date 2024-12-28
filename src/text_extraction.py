6d363d8b8ef058b7be1a5183b2068458f706c6c0
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-28 19:02:22 +0100

    Update text_extraction.py

--- src/text_extraction.py
+++ src/text_extraction.py
@@ -1,10 +1,46 @@
 import json
-from utils import get_text, create_logger, hash_file, hash_str
+import os
+import sys
+from logging import DEBUG
+
+from utils import make_output, get_text, write_table
+import env
 
 
-def main():
+def write_txt(text, output_dir_path, logger):
   pass
 
 
+def clean_source(source_path, logger):
+  pass
+
+
+def extract_corpus(source_path, output_dir_path, logger):
+  logger.info("")
+
+  logger.debug("")
+  source = clean_source(source_path, logger)
+  hashes_table = {}
+  
+  for doc_hash, path in source.items():
+    text = get_text(path, logger)
+    text_hash = write_txt(text, output_dir_path, logger)
+
+    logger.debug("")
+    hashes_table[doc_hash] = text_hash
+
+  logger.debug("")
+  write_table(hashes_table, output_dir_path)
+
+  logger.info("")
+  return 0
+
+
+def main():
+  uuid, output_dir_path, logger = make_output(PATH.OUTPUT)
+  return extract_corpus(PATH.SOURCE, output_dir_path, logger)
+
+
 if __name__ == "__main__":
-  main()
+  exit_code = main()
+  sys.exit(exit_code)
