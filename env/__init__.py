59b7873065f76065da480f79df842bd70078ee6f
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-28 17:25:12 +0100

    Delete env directory

--- env/__init__.py
+++
@@ -1,4 +0,0 @@
-from .path import PATH
-from .environ_vars import ENVIRON_VARS
-
-__all__ = ["PATH", "ENVIRON_VARS"]
--- env/path.py
+++
@@ -1,6 +0,0 @@
-from os.path import abspath
-
-class PATH:
-  CORPUS = abspath("../data/corpus")
-  HASH_TABLE = abspath("../data/source/metadata.json")
-  CACHE = abspath("../.cache")
