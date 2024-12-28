1257679545b10b20b855480a4021319753360652
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-28 19:37:32 +0100

    Update text_extraction.py

--- src/text_extraction.py
+++ src/text_extraction.py
@@ -3,28 +3,24 @@ import os
 import sys
 from logging import DEBUG
 
-from utils import make_output, get_text, write_table
+from utils import make_output, write_file, write_table, get_text
 import env
-
-
-def write_txt(text, output_dir_path, logger):
-  pass
 
 
 def clean_source(source_path, logger):
   pass
 
 
-def extract_corpus(source_path, output_dir_path, logger):
+def extract_corpus(source_path, table_path, files_path, logger):
   logger.info("")
-
+  
   logger.debug("")
   source = clean_source(source_path, logger)
   hashes_table = {}
   
   for doc_hash, path in source.items():
     text = get_text(path, logger)
-    text_hash = write_txt(text, output_dir_path, logger)
+    text_hash = write_file(text, output_dir_path, logger)
 
     logger.debug("")
     hashes_table[doc_hash] = text_hash
