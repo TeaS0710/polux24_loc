b32746ed796a22892ad693568666925dc53d6c6a
Camille /kamij/ <cmonnot.perso@gmail.com>
2024-12-29 20:12:44 +0100

    Update files_manager.py

--- src/utils/files_manager.py
+++ src/utils/files_manager.py
@@ -1,2 +1,17 @@
+import csv
+
 class FileManager:
-    pass
+    @classmethod
+    def create(dir_path, table_header, logger_verbose=True, logger_base_level=logging.DEBUG):
+        uuid = generate_uuid()
+
+        self.path.root = os.path.join(dir_path, uuid)
+        self.path.files = os.path.join(root, "files")
+        self.path.htable = os.path.join(root, "hashes_table.csv")
+        self.path.log = os.path.join(root, "main.log")
+
+        table = [[cell.upper() for cell in table_header]]
+        logger = create_logger(uiid, log, verbose=logger_verbose, base_level=logger_base_level)
+
+        
+        
