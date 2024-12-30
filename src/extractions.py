39b446f9d754243a7b9a5bd279f8f55eec5852ed
TeaS0710 <161449251+TeaS0710@users.noreply.github.com>
2024-12-30 19:00:33 +0100

    Update extractions.py

--- src/extractions.py
+++ src/extractions.py
@@ -1,19 +1,29 @@
-from utils import FileManager, get_text, get_locs
+from utils import FileManager, get_text, get_locs, create_logger
 
 
-def extract_texts(output, source):
-  output.logger.info("Function `extract_texts` called. Start process.")
+def extract_texts(output, source, logger):
+  logger.info("Function `extract_texts` called. Start process.")
   
-  for file in source.files():
-    text = get_text(file.content, output.logger, ext=file.ext)
-    output.write_text(text, table=(file.hash))
+  for file_hash, file_metadata in source:
+    with source.open(file_hash) as file:
+      text = get_text(file.read(), output.logger, ext=file_metadata["ext"])
+      
+    output.write(text, metadata={"source": file_hash})
 
-  output.write_table()
-  output.logger.info("Fuction `extract_texts` done.")
+  logger.info("Fuction `extract_texts` done.")
   
   return 0
 
 
+def main():
+  logger = create_logger("text", "../cache/test.log", verbose=True, base_level=logging.DEBUG)
+  with FileManager("../output/test1", "w+", output_scheme, logger) as output:
+    with FileManager("../data/source", "r", source_scheme, logger) as source:
+      extract_texts(output, source, logger)
+  return 0
+
+
+"""
 def extract_locs(output, texts):
   output.logger.info("Function `extract_locs` called. Start process.")
 
@@ -25,3 +35,4 @@ def extract_locs(output, texts):
   output.logger.info("Fuction `extract_texts` done.")
 
   return 0
+  """
