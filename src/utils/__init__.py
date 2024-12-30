8b54a60be8fe327a80784984f7ce94e0c4e0bee1
TeaS0710 <161449251+TeaS0710@users.noreply.github.com>
2024-12-30 19:21:15 +0100

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,12 +1,13 @@
 from .misc import generate_uuid, hash_file, hash_data, create_logger
-from .get_text import get_text
+from .get_text import pdf_text_extractor, html_text_extractor
 from .get_locs import get_locs
 
 __all__ = [
   "generate_uuid",
   "hash_file",
   "hash_data",
+  "pdf_text_extractor",
+  "html_text_extractor",
   "create_logger",
-  "get_text",
   "get_locs"
 ]
